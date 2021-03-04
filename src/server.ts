234dd185468e5d5c919acda844ae82b853e62047
butsuli-shine <cheichey.1223@gmail.com>
2021-03-04 20:37:39 +0900

    add: メッセージを送るためにAPIをたたく処理を追加した

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,7 @@ const axiosBase = require('axios');
 const line = require('@line/bot-sdk');
 const PORT = process.env.PORT || 3005;
 const config = require('./config');
+const BASE_URL = 'https://https://api-data.line.me/v2/bot/message';
 
 const app = express();
 const getImageEndpoint = axiosBase.create({
@@ -13,7 +14,6 @@ const getImageEndpoint = axiosBase.create({
 });
 
 const postMessageEndpoint = axiosBase.create({
-  baseUrl: 'http://api.line.me/v2/bot/message/reply',
   headers: {
     'Content-Type': 'application/json',
     Authorization: 'Beater ' + config.channelAccessToken,
@@ -23,20 +23,24 @@ const postMessageEndpoint = axiosBase.create({
 console.log('------server is running------');
 app.post('/webhook', line.middleware(config), (req: any, res: any) => {
   getImageEndpoint
-    .get(
-      'https://api-data.line.me/v2/bot/message/' +
-        req.body.events[0].message.id +
-        '/content',
-      {
-        responseType: 'arraybuffer',
-      }
-    )
+    .get(BASE_URL + req.body.events[0].message.id + '/content', {
+      responseType: 'arraybuffer',
+    })
     .then((response: AxiosResponse<any>) => {
       const result = Buffer.from(response.data, 'binary').toString('base64');
+      sendMessage(result, req, ['Hello']);
+      console.log('----POSTED----');
     })
     .catch((error: AxiosResponse<any>) => {
       console.log(error);
     });
 });
+
+const sendMessage = (result: string, req: any, message: [string]) => {
+  postMessageEndpoint.post(BASE_URL + '/reply', {
+    replyToken: req.body.events[0].replyToken,
+    message: message,
+  });
+};
 
 app.listen(PORT);
